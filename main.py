1dd3da936e7dc81e5779f291fe63ae2f5087ae18
Anshu Sinha <anshujuly2@gmail.com>
2023-04-19 20:56:19 +0530

    Add a code to scrap data from python.org

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# ---------------- MODULES ---------------- #
 # imported webdriver class from selenium module
 from selenium import webdriver
 
@@ -10,26 +11,50 @@ from selenium.webdriver.common.by import By
 # import time module
 import time
 
+
+# ---------------- WORKING WITH SELENIUM DRIVER ---------------- #
 # variable to store chrome diver path
 chromeDriverPath = "/Users/anshusinha/Documents/Python/Angela Yu/chromedriver_mac64/chromedriver"
 
 # created a driver object
 driver = webdriver.Chrome(service=Service(chromeDriverPath))
 
+
+# ---------------- SCRAPPING DATA FROM AMAZON ---------------- #
+# # get method to launch the URL
+# driver.get("https://www.amazon.in/Logitech-Master-Advanced-Wireless-Mouse/dp/B084TFH4BN/?_encoding=UTF8&pd_rd_w=denFQ&content-id=amzn1.sym.b5b6da36-128a-4deb-abfd-563ae12c2d96&pf_rd_p=b5b6da36-128a-4deb-abfd-563ae12c2d96&pf_rd_r=A0H30T6P9D47K9AKFJ5Z&pd_rd_wg=7RE1e&pd_rd_r=3ba085bc-a147-47d9-b384-ab88776e0db2&ref_=pd_gw_ci_mcx_mr_hp_atf_m")
+
+# # find the element by class name
+# price = driver.find_element(By.CLASS_NAME, "a-price-whole").text
+
+# # print the price
+# print(price)
+
+# # find the element by xpath  
+# helpLink = driver.find_element(By.XPATH, "//*[@id='navFooter']/div[1]/div/div[7]/ul/li[6]/a").get_attribute("href")
+
+# # print the help link
+# print(helpLink)
+
+
+# ---------------- SCRAPPING DATA FROM PYTHON.ORG ---------------- #
 # get method to launch the URL
-driver.get("https://www.amazon.in/Logitech-Master-Advanced-Wireless-Mouse/dp/B084TFH4BN/?_encoding=UTF8&pd_rd_w=denFQ&content-id=amzn1.sym.b5b6da36-128a-4deb-abfd-563ae12c2d96&pf_rd_p=b5b6da36-128a-4deb-abfd-563ae12c2d96&pf_rd_r=A0H30T6P9D47K9AKFJ5Z&pd_rd_wg=7RE1e&pd_rd_r=3ba085bc-a147-47d9-b384-ab88776e0db2&ref_=pd_gw_ci_mcx_mr_hp_atf_m")
+driver.get("https://www.python.org")
 
-# find the element by class name
-price = driver.find_element(By.CLASS_NAME, "a-price-whole").text
+eventDict = {}
 
-# print the price
-print(price)
+for i in range(1, 6):
+    # find the element by xpath
+    eventDate = driver.find_element(By.XPATH, f"//*[@id='content']/div/section/div[2]/div[2]/div/ul/li[{i}]/time").text
 
-# find the element by xpath  
-helpLink = driver.find_element(By.XPATH, "//*[@id='navFooter']/div[1]/div/div[7]/ul/li[6]/a").get_attribute("href")
+    # find the element by xpath
+    eventName = driver.find_element(By.XPATH, f"//*[@id='content']/div/section/div[2]/div[2]/div/ul/li[{i}]/a").text
 
-# print the help link
-print(helpLink)
+    # add the event date and event name to the dictionary
+    eventDict[i] = {"time": eventDate, "name": eventName}
+
+# print the event dictionary
+print(eventDict)
 
 # quit the browser
 driver.quit()
